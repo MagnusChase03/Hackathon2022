093fa81d73e2539d943e30e615725c97024055fc
Kevin Harvey <kharvey2002@gmail.com>
2022-10-29 18:07:57 -0500

    Init React app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+/*
+Root App file - should contain nothing but navigation and conditional rendering code. No application logic should persist here.
+*/
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      {/* REACT ROUTING HERE */}
+    </div>
+  )
+}
+
+export default App
