a313232672292aa4210c5e830fd4eaa7c1024a52
kevinharv <kharvey2002@gmail.com>
2022-11-08 18:50:15 -0600

    Fixing the navbar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles/Header.css';
+
+export default function Header(props) {
+    return (
+        <>
+            <header className="siteHeader">
+                <section className="navbar">
+                    <div className="leftEdge"><h2>HackUTD IX</h2></div>
+                    {/* THE BELOW LINE CAN BE USED FOR THE CENTER TITLE */}
+                    <div className="centerEdge"><h2>PLACEHOLDER</h2></div>
+                    <div className="rightEdge">
+                        <button className="logoutButton" onClick={() => { alert("LOGGED OUT") }}>LOGOUT</button>
+                        <h3 className="username" >USERNAME</h3>
+                    </div>
+                </section>
+
+            </header>
+        </>
+    );
+}
