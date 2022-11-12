57c5450b4c4ea57063a0f9c6e335363cad28a68e
Zachary <zachness2003@gmail.com>
2022-11-12 14:47:28 -0600

    Starting python data collection

--- backend/node/src/dataScience/main.py
+++ backend/node/src/dataScience/main.py
@@ -1,6 +1,21 @@
 import numpy as np
+import pymongo as MongoClient
+import os
+
+#API Key K2ZYSZNRVUVWQF2K
+# https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=K2ZYSZNRVUVWQF2K
+
+def connectDB():
+    CONNECTION_STRING = 'mongodb://root:' + os.MONGO_INITDB_ROOT_PASSWORD + '@mongodb:27017/?authSource=admin'
+    client = MongoClient(CONNECTION_STRING)
+    return client
+
+def getData():
+    database = connectDB()
+
+    
 
 def main():
-    pass
+    getData()
 
 main()
