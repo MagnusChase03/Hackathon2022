5c5871a95e16d388461dcd126b40daf4fa9e8185
kevinharv <kharvey2002@gmail.com>
2022-11-12 22:14:31 -0600

    Modified some CSS

--- frontend/src/components/ProfileForm.jsx
+++ frontend/src/components/ProfileForm.jsx
@@ -1,226 +1,188 @@
-
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Radio, FormControlLabel, MenuItem } from '@mui/material';
 import { RadioGroup, TextField } from '@mui/material';
-// import {createTheme} from '@mui/material/styles';
+import { useFormik } from 'formik';
 import '../styles/ProfileForm.css'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-
-let theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1ce1ce',
-    },
-    secondary: {
-      main: '#edf2ff',
-    },
-  },
-});
-
-theme = createTheme(theme, {
-  palette: {
-    info: {
-      main: theme.palette.secondary.main,
-    },
-  },
-});
 
 
 export default function ProfileForm(props) {
- 
-    return (
-      
-      <Formik
-      initialValues={{
-        investments: '',
-        disposableIncomeBracket: '',
-        emergencyAccess: '',
-        liquidityPreference: '',
-        investmentLength: ''
-        
-      }}
-      onSubmit={async (values) => {
-        props.handler(values);
-      }}
-    >
+  const formik = useFormik({
+    initialValues: {
+      username: 'Test User',
+      currentInvestment: '',
+      financialGoal: '',
+      disposableIncomeBracket: '',
+      emergencyAccess: '',
+      liquidityPreference: '',
+      investmentLength: '',
+      publicStockPercent: '',
+      privateStockPercent: '',
+      bondsPercent: '',
+      cryptoPercent: '',
+      forexPercent: '',
+    },
 
-      {({ values }) => (
-        
-        
-        <Form id='form'>
+    onSubmit: values => {
+      props.handler(values);
+    },
+  });
+  return (
+    <>
+    <form onSubmit={formik.handleSubmit}>
 
-<ThemeProvider theme={theme}>
+      <h3 className='question'>Do you have any current investments?</h3>
+      <RadioGroup name="currentInvestment" onChange={formik.handleChange} color='primary'>
+        <FormControlLabel
+          value="Yes"
+          control={<Radio color='primary' />}
+          label="Yes"
+          color='secondary'
+        />
 
-            <h3 className='question'>Do you have any current investments?</h3>
-            <Field component={RadioGroup} name="currentInvestment">
-              <FormControlLabel
-                value="Yes"
-                control={<Radio />}
-                label="Yes"
-                color='primary'
-              />
-             
-              <FormControlLabel
-                value="No"
-                control={<Radio />}
-                label="No"
-                color='primary'
-              />
-            </Field>
-
-            <h3 className='question'>If you do, what are they?</h3>
-            <TextField
-              fullWidth
-              id="publicStockPercent"
-              name="publicStockPercent"
-              label="% Public Stocks"
-              placeholder='20'
-              value={values.publicStockPercent}
-            />
-            <TextField
-              fullWidth
-              id="privateStockPercent"
-              name="privateStockPercent"
-              label="% Private Stocks"
-              placeholder='20'
-              value={values.privateStockPercent}
-            />
-            <TextField
-              fullWidth
-              id="bondsPercent"
-              name="bondsPercent"
-              label="% Government Bonds"
-              placeholder='20'
-              value={values.bondsPercent}
-            />
-            <TextField
-              fullWidth
-              id="cryptoPercent"
-              name="cryptoPercent"
-              label="% Cryptocurrency"
-              placeholder='20'
-              value={values.cryptoPercent}
-            />
-            <TextField
-              fullWidth
-              id="forexPercent"
-              name="forexPercent"
-              label="% Foreign Exchange"
-              placeholder='20'
-              value={values.forexPercent}
-            />
-          
-
-          <h3 className='question'>How much are you thinking about investing?</h3>
-            <TextField
-              fullWidth
-              select
-              // component={TextField} 
-              id="disposableIncomeBracket"
-              value={values.disposableIncomeBracket}
-              helperText="Disposable Income Bracket"
-              onChange={console.log(values.disposableIncomeBracket)}
-
-            >
-
-              <MenuItem key="10" value="0-5000">
-                Under $5,000
-              </MenuItem>
-              <MenuItem key="10" value="5000-10000">
-                $5,000 - $10,000
-              </MenuItem>
-              <MenuItem key="10" value="10001-20000">
-                $10,001 - $20,000 
-              </MenuItem>
-              <MenuItem key="10" value="20001-50000">
-                $20,001 - $50,000
-              </MenuItem>
-              <MenuItem key="10" value="50001-100000">
-                $50,001 - $100,000
-              </MenuItem>
-              <MenuItem key="10" value="100000-999999">
-                More than $100,000
-              </MenuItem>
-              </Field>
+        <FormControlLabel
+          value="No"
+          control={<Radio />}
+          label="No"
+        />
+      </RadioGroup>
 
 
+      {(formik.values.currentInvestment == "Yes") && 
 
-          <h3 className='question'>In case of emergency, do you need access to your funds?</h3>
-          <Field component={RadioGroup} name="emergencyAccess">
-              <FormControlLabel
-                value="Yes"
-                control={<Radio />}
-                label="Yes"
-                color='primary'
+      <div className='currentInvestmentQuestions'>
+      <h3 className='question'>If you do, what are they?</h3>
+      <TextField
+        id="publicStockPercent"
+        name="publicStockPercent"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.publicStockPercent}
+        helperText="% Public Stocks"
+      />
+      <TextField
+        id="privateStockPercent"
+        name="privateStockPercent"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.privateStockPercent}
+        helperText="% Private Stocks"
+      />
+      <TextField
+        id="bondsPercent"
+        name="bondsPercent"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.bondsPercent}
+        helperText="% Government Bonds"
+      />
+      <TextField
+        id="cryptoPercent"
+        name="cryptoPercent"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.cryptoPercent}
+        helperText="% Cryptocurrency"
+      />
+      <TextField
+        id="forexPercent"
+        name="forexPercent"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.forexPercent}
+        helperText="% Foreign Currency Exchange"
+      />
+      </div>}
 
-              />
-              <FormControlLabel
-                value="No"
-                control={<Radio />}
-                label="No"
-                color='primary'
 
-              />
-            </Field>
+      <h3 className='question'>How much are you thinking about investing?</h3>
+      <TextField
+        fullWidth
+        select
+        id="disposableIncomeBracket"
+        name="disposableIncomeBracket"
+        value={formik.values.disposableIncomeBracket}
+        helperText="Disposable Income Bracket"
+        onChange={formik.handleChange}
+      >
+        <MenuItem key="10" value="0-5000">
+          Under $5,000
+        </MenuItem>
+        <MenuItem key="10" value="5000-10000">
+          $5,000 - $10,000
+        </MenuItem>
+        <MenuItem key="10" value="10001-20000">
+          $10,001 - $20,000
+        </MenuItem>
+        <MenuItem key="10" value="20001-50000">
+          $20,001 - $50,000
+        </MenuItem>
+        <MenuItem key="10" value="50001-100000">
+          $50,001 - $100,000
+        </MenuItem>
+        <MenuItem key="10" value="100000-999999">
+          More than $100,000
+        </MenuItem>
+      </TextField>
+
+      <h3 className='question'>In case of emergency, do you need access to your funds?</h3>
+      <RadioGroup name="emergencyAccess" onChange={formik.handleChange}>
+        <FormControlLabel
+          value="Yes"
+          control={<Radio />}
+          label="Yes"
+          color='primary'
+        />
+
+        <FormControlLabel
+          value="No"
+          control={<Radio />}
+          label="No"
+          color='primary'
+        />
+      </RadioGroup>
 
 
-          <h3 className='question'> What percentage of investment would you need access to in case of emergency?</h3>
-          <TextField
-              fullWidth
-              id="liquidityPreference"
-              name="liquidityPreference"
-              label="% Liquidity Preference"
-              placeholder='20'
-              value={values.liquidityPreference}
-            />
+      {(formik.values.emergencyAccess) &&
+      <div className='emergencyAccessSection'>
+        <h3 className='question'>What percentage of investment would you need access to in case of emergency?</h3>
+        <TextField
+          id="liquidityPreference"
+          name="liquidityPreference"
+          type="text"
+          placeholder='20'
+          onChange={formik.handleChange}
+          value={formik.values.liquidityPreference}
+          helperText="% Liquid Access"
+        />
+      </div>}
 
-          <h3 className='question'>What are your financial goals?</h3>
-          <TextField
-              fullWidth
-              id="financialGoal"
-              name="financialGoal"
-              label="% Financial Goal"
-              placeholder='20'
-              value={values.financialGoal}
-            />
+      <h3 className='question'>What are your financial goals?</h3>
+      <TextField
+        id="financialGoal"
+        name="financialGoal"
+        type="text"
+        placeholder='15'
+        onChange={formik.handleChange}
+        value={formik.values.financialGoal}
+        helperText="% Financial Gain Goal"
+      />
 
-          <h3 className='question'>How long are you looking to invest?</h3>
-          <TextField
-              fullWidth
-              id="investmentLength"
-              name="investmentLength"
-              label="% Investment Length"
-              placeholder='20'
-              value={values.investmentLength}
-            />
+      <h3 className='question'>How long are you looking to invest?</h3>
+      <TextField
+        id="investmentLength"
+        name="investmentLength"
+        type="text"
+        placeholder='4'
+        onChange={formik.handleChange}
+        value={formik.values.investmentLength}
+        helperText="Investment Length (Years)"
+      />
 
-        <button type="submit" id='submit'>Submit</button>
-        </ThemeProvider>
+      <br />
 
-        </Form>
-      )}
-    </Formik>
-    );
+      <button type="submit">Submit</button>
+    </form>
+    </>
+  );
+
 }
-// Do you have any current investments? y/n
-// if you do what are they? return json object with all investments and percentages invested
-// how much money are you thinking of investing? return something that indicates the bracket of investment that we can use.
-// In case of emergency will you need access to your investments? yes/no
-// what percentage of your investment do you need access to in case of an emergency? integer representing the percentage
-// What is your financial goal? percentage of growth as int
-// How long are you looking to invest? date time object or something
-
-// var newUserProfile = {
-//   "username": req.body.username,
-//   "publicStockPercent": parseDouble(req.body.publicStockPercent),
-//   "privateStockPercent": parseDouble(req.body.privateStockPercent),
-//   "bondsPercent": parseDouble(req.body.bondsPercent),
-//   "cryptoPercent": parseDouble(req.body.cryptoPercent),
-//   "forexPercent": parseDouble(req.body.forexPercent),
-//   "liquidityPrefrence": req.body.liquidityPrefrence,
-//   "investmentLength": parseInt(req.body.investmentLength),
-//   "disposableIncomeBracket": req.body.disposableIncomeBracket,
-//   "financialGoal": req.body.financialGoal
-//   };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@
 
 import { React, useState } from 'react';
 import Header from '../components/Header';
-import  NewForm from '../components/NewForm';
+import ProfileForm from '../components/ProfileForm';
 import '../styles/Home.css';
 
 export default function Home() {
@@ -47,7 +47,7 @@ export default function Home() {
     return(
         <div className='homeDiv'>
             <Header message="Investment Buddy"/>
-            {(!formSubmitted) && <NewForm handler={handleFormSubmission} />}
+            {(!formSubmitted) && <ProfileForm handler={handleFormSubmission} />}
 
             {formSubmitted && !dataReady && <h1>LOADING...</h1>}
             {formSubmitted && dataReady && <h1>DATA</h1>}
